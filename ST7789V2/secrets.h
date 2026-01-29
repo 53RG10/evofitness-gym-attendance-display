c485e5f9c0a99cb35a1cfc05ad73739aedcf2dfa
Sergio Guarino <sergio.guarino@sib.swiss>
2026-01-29 15:10:36 +0100

    added secrets.h file

---
+++ ST7789V2/secrets.h
@@ -0,0 +1,12 @@
+// WiFi credentials
+// IMPORTANT: This file contains sensitive information.
+// DO NOT commit this file to version control!
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// -------- WiFi configuration --------
+const char* SECRET_SSID = "WiFi";
+const char* SECRET_PASS = "Password";
+
+#endif
